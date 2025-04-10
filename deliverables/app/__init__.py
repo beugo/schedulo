85215fe3c3330e3f0186b921c7cd64abcdae4843
Joel Smith <joelsmith2004@gmail.com>
2025-04-10 21:03:21 +0800

    + Added login protected routes, ability to login, logout, register a user, added table users, added tailwindcss cdn and some basic styling

--- deliverables/app/__init__.py
+++ deliverables/app/__init__.py
@@ -1,15 +1,25 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 db = SQLAlchemy()
+login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+    app.config['SECRET_KEY'] = 'password123'  
     
     db.init_app(app)
+    login_manager.init_app(app)
+    
+    login_manager.login_view = 'main.login'  
+    login_manager.login_message_category = 'info'  
 
     from .routes import main
     app.register_blueprint(main)
+
+    with app.app_context():
+        db.create_all()
 
     return app
