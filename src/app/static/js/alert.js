7fa7741fca6f2d5c78b0dd0419ef6c66fa3cab5f
Joel Smith <joelsmith2004@gmail.com>
2025-04-14 19:39:29 +0800

    + Login and register css improvements, + update alerts to go over the top of the page and to fade out after 5s

---
+++ src/app/static/js/alert.js
@@ -0,0 +1,16 @@
+window.addEventListener('load', () => {
+    const alertContainer = document.querySelector('ul.p-3');
+    const alerts = alertContainer.querySelectorAll('.alert');
+
+    alerts.forEach(alert => {
+        setTimeout(() => {
+            alert.classList.add('fade-out');
+            alert.addEventListener('animationend', () => {
+                alert.remove();
+                if (!alertContainer.querySelector('.alert')) {
+                    alertContainer.classList.remove('p-3');
+                }
+            });
+        }, 5000);
+    });
+});
