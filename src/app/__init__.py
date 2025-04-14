62eaf6ae5b7c1c4ea2ba58ed82a8429eb1f1c529
Joel Smith <joelsmith2004@gmail.com>
2025-04-14 15:48:06 +0800

    + fix .env file getter

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,11 +1,12 @@
 import os
-
+from dotenv import load_dotenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 db = SQLAlchemy()
 login_manager = LoginManager()
+load_dotenv()
 
 def create_app():
     app = Flask(__name__)
