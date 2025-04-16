d08a9b80e311019b6d39a376738dd01361b72dd6
Quarterpie3141 <prashan.csgo@gmail.com>
2025-04-16 11:46:09 +0800

    added the basse styles/colors used for the site

---
+++ src/app/static/js/tailwindconfig.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+    darkMode: 'class',
+    theme: {
+      extend: {
+        colors: {
+          dark: {
+            bg: '#121212',
+            fg: '#181818',
+            primary: '#E0E0E0',
+            secondary: '#B0B0B0',
+            border: '#343434',
+            accent: '#888888',
+          }
+        }
+      }
+    }
+  }
