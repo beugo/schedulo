0a81d1c00216dc1a8d34e194a03c6ed3567f0b3c
Hugo Smith <huga@iinet.net.au>
2025-04-29 22:31:57 +0800

    Split routes.py into blueprints - created blueprints directory with new files - modified __init__.py to import from blueprints/ - modified templates to use new blueprints - altered base.html tags to use relative paths

---
+++ src/app/blueprints/__init__.py
@@ -0,0 +1,5 @@
+from .auth import auth_bp
+from .main import main_bp
+from .units import units_bp
+from .plans import plans_bp
+from .friends import friends_bp
--- src/app/models.py
+++ src/app/models.py
@@ -1,7 +1,8 @@
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
-from . import db
 
+from app import db
 
 class User(UserMixin, db.Model):
     """User model for authentication and account data."""
