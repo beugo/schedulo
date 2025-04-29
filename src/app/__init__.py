2af4c9c63d14017acfe4dda51d5e275e0d351a2c
Hugo Smith <huga@iinet.net.au>
2025-04-29 20:57:46 +0800

    Add config.py

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -4,6 +4,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
+from .config import Config
+
 db = SQLAlchemy()
 login_manager = LoginManager()
 load_dotenv()
@@ -12,8 +14,7 @@ load_dotenv()
 def create_app():
     """Create and configure the Flask application."""
     app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
-    app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+    app.config.from_object(Config)
 
     db.init_app(app)
     login_manager.init_app(app)
