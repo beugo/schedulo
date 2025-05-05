4c54bad1a4fae937df3e6aa2dd9a2c6647249925
Hugo Smith <huga@iinet.net.au>
2025-05-05 23:47:11 +0800

    Fix friend request routing errors - Changed blueprint definition from FriendRequests to friend_requests   so that it matched the rest of the code - Fixed minor variable syntax error in friendrequests.py

--- src/app/blueprints/__init__.py
+++ src/app/blueprints/__init__.py
@@ -3,3 +3,4 @@ from .main import main_bp
 from .units import units_bp
 from .plans import plans_bp
 from .friends import friends_bp
+from .friendrequests import friend_req_bp
--- src/app/blueprints/friendrequests.py
+++ src/app/blueprints/friendrequests.py
@@ -3,10 +3,10 @@ from flask_login import current_user
 from app import db
 from app.models import User, UserFriend, FriendRequests
 
-friend_req = Blueprint("FriendRequests", __name__)
+friend_req_bp = Blueprint("friend_requests", __name__)
 
 
-@friend_req.route("/get", methods=["GET"])
+@friend_req_bp.route("/get", methods=["GET"])
 def get_friend_requests():
     if not current_user.is_authenticated:
         return jsonify({"ok": False}), 401
@@ -19,7 +19,7 @@ def get_friend_requests():
     )
     return jsonify([ {"username": user.username, "created_at": created_at or "Not Found?"} for user, created_at in links ])
 
-@friend_req.route("/send", methods=["POST"])
+@friend_req_bp.route("/send", methods=["POST"])
 def send_friend_request():
     """Add a friend to the current user."""
     friend_username = request.json.get("q", "")
@@ -33,7 +33,7 @@ def send_friend_request():
     existing_friendship = UserFriend.query.filter_by(
         user_id=current_user.id, friend_id=friend.id
     ).union(UserFriend.query.filter_by(
-        user_id=friend.id, friend_id=cure.id
+        user_id=friend.id, friend_id=current_user.id
     )).first()
     if existing_friendship:
         return jsonify({"message": "Already previously friends", "ok": False}), 400
@@ -52,7 +52,7 @@ def send_friend_request():
 
     return jsonify({"message": "Sent friend request successfully", "ok": True}), 200
 
-@friend_req.route("/remove", methods=["PATCH"])
+@friend_req_bp.route("/remove", methods=["PATCH"])
 def remove_friend_request():
     """Remove friend request"""
     friend_username = request.json.get("q", "")
