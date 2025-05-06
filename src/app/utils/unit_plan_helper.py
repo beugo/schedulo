c238b3615160ae77b77d37c899dbf72e1f9d4163
Joel Smith <joelsmith2004@gmail.com>
2025-05-06 09:58:25 +0800

    + missed file
    added helper function

--- src/app/utils/unit_plan_helper.py
+++ src/app/utils/unit_plan_helper.py
@@ -1,18 +1,15 @@
 # app/utils/unit_plan_helper.py
-
 from app.models import UnitPlan, UnitPlanToUnit, Unit
 from app import db
 
-
-# extracted core query im sick of rewriting queries
 def get_plan_core_info(plan_id, user_id):
     plan = db.session.query(UnitPlan).filter_by(id=plan_id, user_id=user_id, is_deleted=False).first()
     if not plan:
         return None
 
-    unitsToUnitPlans = db.session.query(UnitPlanToUnit).filter_by(unit_plan_id=plan.id, is_deleted=False).all()
+    upToUnit = db.session.query(UnitPlanToUnit).filter_by(unit_plan_id=plan.id, is_deleted=False).all()
     units = []
-    for u in unitsToUnitPlans:
+    for u in upToUnit:
         unit = db.session.query(Unit).filter_by(id=u.unit_id, is_deleted=False).first()
         if unit:
             units.append({
@@ -27,8 +24,3 @@ def get_plan_core_info(plan_id, user_id):
         "name": plan.name,
         "units": units
     }
-
-
-
-
-
