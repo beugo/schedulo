3955a3b23728f129adddb407fca4341da3211d92
Hugo Smith <huga@iinet.net.au>
2025-05-11 18:36:50 +0800

    Change test_selenium to use a thread instead of a process

--- src/app/tests/test_selenium.py
+++ src/app/tests/test_selenium.py
@@ -1,39 +1,52 @@
+import time
+import threading
 import unittest
-import multiprocessing
-from selenium import webdriver
 
-import selenium.webdriver
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
 from app import create_app, db
 from app.config import TestConfig
 from app.seeds import import_units, create_users_and_plans
 
 LOCALHOST = "http://localhost:5000/"
+CSV_PATH = "../data-scraping/cits.csv"
+
+def _run_app(app):
+    app.run(port=5000, use_reloader=False)
 
 class SeleniumTests(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        options = webdriver.ChromeOptions()
+        options.add_argument("--headless=new")
+        cls.driver = webdriver.Chrome(options=options)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+
     def setUp(self):
-        self.testApp = create_app(TestConfig)
-        self.app_context = self.testApp.app_context()
+        self.app = create_app(TestConfig)
+        self.app.testing = True
+        self.app_context = self.app.app_context()
         self.app_context.push()
         db.create_all()
 
-        import_units(db.session, "../data-scraping/cits.csv")
+        import_units(db.session, CSV_PATH)
         create_users_and_plans(db.session)
 
-        self.server_thread = multiprocessing.Process(target=self.testApp.run)
-        self.server_thread.start()
-
-        options = webdriver.ChromeOptions()
-        options.add_argument("--headless=new")
-        self.driver = webdriver.Chrome(options=options)
+        self.server = threading.Thread(
+            target=_run_app, args=(self.app,), daemon=True
+        )
+        self.server.start()
+        time.sleep(1)
 
     def tearDown(self):
         db.session.remove()
         db.drop_all()
         self.app_context.pop()
-
-        self.server_thread.terminate()
-        self.driver.close()
 
     def test_homepage_loads_and_has_title(self):
         self.driver.get(LOCALHOST)
